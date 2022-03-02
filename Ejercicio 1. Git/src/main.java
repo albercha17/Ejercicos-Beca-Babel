db7b5ff8e5a798bdfe87e1f6722b688a425e2e6c
albercha17 <alberchaucm.es>
2022-03-02 18:59:58 +0100

    error

--- Ejercicio 1. Git/src/main.java
+++ Ejercicio 1. Git/src/main.java
@@ -70,6 +70,7 @@ public class main {
 			System.out.println("1.Sumar.");
 		    System.out.println("2.Restar.");
 		    System.out.println("3.Salir.");
+		    System.out.println("4.Error.");
 	     	z = sc.nextLine();}
 	    else if(Integer.parseInt(z)==2) {
 	    	restar ( resta);
